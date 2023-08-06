9f90be38ff9b6e4d940ef3a9129cba7ac12b6e08
y-skindersky <y.skindersky@gmail.com>
2023-08-06 16:44:36 +0400

    Course schedule
    Course schedule

---
+++ GraphTheory.Tests/CourseScheduleTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace GraphTheory.Tests
+{
+    public class CourseScheduleTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var result = CourseSchedule.CanFinish(2, new[]
+            {
+                new[] { 0, 1 }
+            });
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = CourseSchedule.CanFinish(2, new[]
+            {
+                new[] { 1, 0 },
+                new[] { 0, 1 }
+            });
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var result = CourseSchedule.CanFinish(1, new int[0][]);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var result = CourseSchedule.CanFinish(3, new[]
+            {
+                new[] { 1, 0 },
+                new[] { 1, 2 },
+                new[] { 0, 1 }
+            });
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ GraphTheory/CourseSchedule.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GraphTheory
+{
+    public static class CourseSchedule
+    {
+        public static bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            var nextCourses = new List<short>[numCourses];
+            var prevCourses = new List<short>[numCourses];
+            for (int i = 0; i < numCourses; i++)
+            {
+                nextCourses[i] = new List<short>();
+                prevCourses[i] = new List<short>();
+            }
+            foreach (var prerequisite in prerequisites)
+            {
+                var course = (short)prerequisite[0];
+                var reqCourse = (short)prerequisite[1];
+
+                nextCourses[reqCourse].Add(course);
+                prevCourses[course].Add(reqCourse);
+            }
+
+            short finished = 0;
+            while (finished < numCourses)
+            {
+                List<short> queue = new List<short>();
+                for (short i = 0; i < numCourses; i++)
+                {
+                    if (nextCourses[i] == null || prevCourses[i].Count > 0)
+                        continue;
+                    finished++;
+                    queue.Add(i);
+                }
+
+                if (queue.Count == 0)
+                    return false;
+
+                foreach (short course in queue)
+                {
+                    if (nextCourses[course] == null)
+                        continue;
+
+                    foreach (var nextCourse in nextCourses[course])
+                    {
+                        if (!prevCourses[nextCourse].Remove(course))
+                            return false;
+                    }
+
+                    nextCourses[course] = null;
+                }
+            }
+
+            return finished == numCourses;
+        }
+    }
+}
